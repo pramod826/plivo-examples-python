d2ec104afb9e4d691d3ed3df85cdb10f1b2be451
Pramod <pramod@plivo.com>
2013-08-11 18:42:59 +0530

    demo IVR app added

---
+++ demo_IVR_app/speak_digit.py
@@ -0,0 +1,24 @@
+from flask import Flask, request,make_response,Response
+import plivo
+import os
+app = Flask(__name__)
+
+@app.route('/getdigits',methods=['GET','POST'])
+def getdigits():
+	if request.method == 'GET':
+		digits=request.args.get('Digits','')
+	elif request.method == 'POST':
+		digits=request.args.get('Digits','')
+
+	xml=plivo.XML.Response()
+	if digits:
+		xml.addSpeak('You have Pressed '+ str(digits))
+	else:
+		xml.addSpeak('You have not pressd any digit')
+
+	return Response(xml.to_xml(),mimetype='text/xml')
+ 
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT',5079))
+    app.run(host='0.0.0.0' , port=port)
